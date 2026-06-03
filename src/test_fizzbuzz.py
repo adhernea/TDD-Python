86b6353a9952ee50372599b3e62fb10307b041aa
Adrian HC <adrian.herrera@salesianos-lacuesta.net>
2026-06-03 08:16:47 +0100

    test: fizzbuzz input 3, 5 and 15

--- src/test_fizzbuzz.py
+++ src/test_fizzbuzz.py
@@ -9,3 +9,12 @@ def when_zero_give_empty_string():
 def when_one_give_empty_string():
     assert fizz_buzz(1) == ""
 
+def when_three_give_fizz():
+    assert fizz_buzz(3) == "Fizz"
+
+def when_five_give_buzz():
+    assert fizz_buzz(5) == "Buzz"
+
+def test_fizz_buzz_fifteen():
+    assert fizz_buzz(15) == "FizzBuzz"
+
