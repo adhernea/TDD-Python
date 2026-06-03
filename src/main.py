548f362463212b17d1df9d4384d988940ee30439
Adrian HC <adrian.herrera@salesianos-lacuesta.net>
2026-06-03 11:00:48 +0100

    docs(main): fizz_buzz explanation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 print('Hello world!')
 
+"""
+    Esta función recibe un número entero y devuelve Fizz si el número es múltiplo de 3, Buzz si es múltiplo de 5 y FizzBuzz si es múltiplo de ambos.
+    En caso de tener una entrada de 1 o 0 retornará un string vacío.
+    En caso de pasar un número que no sea ningún múltiplo de 3 o 5 retornará el número en cuestión.
+"""
 def fizz_buzz(number: int):
     if not isinstance(number, int) or number == 0 or number == 1:
         return ""
@@ -12,4 +17,3 @@ def fizz_buzz(number: int):
         return "Buzz"
     
     return number
-    
