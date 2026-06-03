cf434fe5a314b98514d4c315dcd5a012c936461f
Adrian HC <adrian.herrera@salesianos-lacuesta.net>
2026-06-03 09:37:20 +0100

    feat: fiz_buzz implementation for 3, 5 and 15 inputs

--- src/main.py
+++ src/main.py
@@ -4,5 +4,12 @@ def fizz_buzz(number: int):
     if number == 0:
         return ""
     
+    if number%3 == 0 and number%5 == 0:
+        return "FizzBuzz"
+    elif number%3 == 0:
+        return "Fizz"
+    elif number%5 == 0:
+        return "Buzz"
+    
     return ""
     
