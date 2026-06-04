31ab3696bf4a4d2de82acc0e507ce49cf72bf656
Adrian HC <adrian.herrera@salesianos-lacuesta.net>
2026-06-04 15:36:52 +0100

    test: added collection with numbers and words

--- src/test_suma_comma_collection.py
+++ src/test_suma_comma_collection.py
@@ -6,6 +6,9 @@ def test_when_111_give_3():
 def test_when_123_give_3():
     assert sum_comma_collection("1,2,3") == 6
 
+def test_when_1tal2_give_0():
+    assert sum_comma_collection("1,tal,2") == 0
+
 def test_when_hello_give_0():
     assert sum_comma_collection("hola") == 0
 
